388071a90642c364dc8393afaad4ffbf9c32b9fe
Steve <schan@apptsunami.com>
2013-05-26 20:12:43 -0700

    Call databaseOpen and databaseClose outside the for loop to be more efficient

--- src/com/apptsunami/samples/yelpdatachallenge/LoadData.java
+++ src/com/apptsunami/samples/yelpdatachallenge/LoadData.java
@@ -4,6 +4,12 @@ import java.io.File;
 
 import com.apptsunami.samples.yelpdatachallenge.parse.ProcessJsonFile;
 
+/**
+ * Main program to load a database from yelp data files
+ * 
+ * @author stevec
+ * 
+ */
 public class LoadData {
 
 	/**
@@ -11,13 +17,13 @@ public class LoadData {
 	 * @param fileName
 	 * @return mongo collection name
 	 * 
-	 * Generate a collection name based on the file name.  For example:
-	 * yelp_academic_dataset_business.json => business
-	 * yelp_academic_dataset_checkin.json => checkin
+	 *         Generate a collection name based on the file name. For example:
+	 *         yelp_academic_dataset_business.json => business
+	 *         yelp_academic_dataset_checkin.json => checkin
 	 * 
 	 */
 	private static String getCollectionNameFromFileName(String fileName) {
-		String str = fileName.substring(fileName.lastIndexOf("_")+1);
+		String str = fileName.substring(fileName.lastIndexOf("_") + 1);
 		return str.substring(0, str.indexOf("."));
 	} // getCollectionNameFromFileName
 
@@ -25,23 +31,31 @@ public class LoadData {
 	 * 
 	 * @param args
 	 * 
-	 * Main program to load data files.  Specify each json file as an argument.
+	 *            Main program to load data files. Specify each json file as an
+	 *            argument. For example:
+	 * 
+	 *            java -jar yelpdatachallenge.load.jar \
+	 *            ../data/yelp_academic_dataset_business.json \
+	 *            ../data/yelp_academic_dataset_checkin.json \
+	 *            ../data/yelp_academic_dataset_review.json \
+	 *            ../data/yelp_academic_dataset_user.json
 	 */
 	public static void main(String[] args) {
 		System.out.println("start");
 		ProcessJsonFile parser = new ProcessJsonFile();
-		for (int i = 0; i < args.length; i++) {
-			System.out.println("loading file " + i + ": " + args[i]);
-			String fileName = args[i];
-			File inputFile = new File(fileName);
-			try {
+		try {
+			parser.openDatabase();
+			for (int i = 0; i < args.length; i++) {
+				System.out.println("loading file " + i + ": " + args[i]);
+				String fileName = args[i];
+				File inputFile = new File(fileName);
 				parser.processFile(inputFile,
 						getCollectionNameFromFileName(fileName));
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} // for
+			} // for
+			parser.closeDatabase();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println("finish");
 	} // main
 
