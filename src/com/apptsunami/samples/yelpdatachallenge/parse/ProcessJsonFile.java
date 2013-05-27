8dc8edcbc94d9751f6c0f1dd49d46e82dc28af60
Steve <schan@apptsunami.com>
2013-05-26 20:12:17 -0700

    Factor out database operation logic to parent class DatabaseOperation.java

--- src/com/apptsunami/samples/yelpdatachallenge/parse/ProcessJsonFile.java
+++ src/com/apptsunami/samples/yelpdatachallenge/parse/ProcessJsonFile.java
@@ -4,26 +4,25 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+import com.apptsunami.samples.yelpdatachallenge.DatabaseOperation;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-import com.mongodb.WriteConcern;
 import com.mongodb.util.JSON;
 
-public class ProcessJsonFile {
-
-	private static final String DB_HOST = "localhost";
-	private static final int DB_PORT = 27017;
-	private static final String DB_NAME = "yelpdatachallenge";
-	private static final String DB_USER = null;
-	private static final String DB_PASSWORD = null;
+/**
+ * This class loads data from yelp json files
+ * 
+ * @author stevec
+ *
+ */
+public class ProcessJsonFile extends DatabaseOperation {
 
 	/* constructors */
 	public ProcessJsonFile() {
 		/* no-op */
-	} // JsonFileParser
+	} // ProcessJsonFile
 
 	/**
 	 * 
@@ -31,8 +30,8 @@ public class ProcessJsonFile {
 	 * @param collectionName
 	 * @return number of records found in the collection
 	 * 
-	 * This function is for debugging purposes.  It prints every record in a
-	 *   collection and returns the number of records found.
+	 *         This function is for debugging purposes. It prints every record
+	 *         in a collection and returns the number of records found.
 	 */
 	private int dumpCollection(DB db, String collectionName) {
 		DBCollection coll = db.getCollection(collectionName);
@@ -45,11 +44,12 @@ public class ProcessJsonFile {
 		DBCursor cursorDoc = coll.find();
 		int count = 0;
 		while (cursorDoc.hasNext()) {
-			System.out.println(cursorDoc.next());
+			// System.out.println(cursorDoc.next());
+			cursorDoc.next();
 			count++;
 		} // while
 		cursorDoc.close();
-		System.out.println("====");
+		System.out.println("==== " + count + " ====");
 		return count;
 	} // dumpCollection
 
@@ -59,23 +59,14 @@ public class ProcessJsonFile {
 	 * @param collectionName
 	 * @throws Exception
 	 * 
-	 * Reads a file where each line is a json object.  The json will be
-	 * loaded as a record into the collection.  It then dumps all records
-	 * in the collection and verifies the number of records equals the
-	 * number of lines read in the file.
+	 *             Reads a file where each line is a json object. The json will
+	 *             be loaded as a record into the collection. It then dumps all
+	 *             records in the collection and verifies the number of records
+	 *             equals the number of lines read in the file.
 	 * 
 	 */
 	public void processFile(File inputFile, String collectionName)
 			throws Exception {
-		MongoClient mongoClient = new MongoClient(DB_HOST, DB_PORT);
-		DB db = mongoClient.getDB(DB_NAME);
-		if ((DB_USER != null) && (DB_PASSWORD != null)) {
-			boolean auth = db.authenticate(DB_USER, DB_PASSWORD.toCharArray());
-			if (!auth) {
-				throw new Exception("Incorrect user name or password");
-			}
-		} // if
-		mongoClient.setWriteConcern(WriteConcern.JOURNALED);
 
 		DBCollection coll = db.getCollection(collectionName);
 		coll.drop();
@@ -96,8 +87,7 @@ public class ProcessJsonFile {
 					+ " from data file but retrieved " + dumpCount
 					+ " objects in collection " + collectionName);
 		}
-		/* done */
-		mongoClient.close();
+
 	} // processFile
 
 } // JsonFileParser
